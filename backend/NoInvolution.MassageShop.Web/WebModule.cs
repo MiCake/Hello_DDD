ce08224ce924854cbd389adccc2a25503b52fdd2
uoyoCsharp <344481481@qq.com>
2021-12-03 17:52:31 +0800

    init repo

---
+++ backend/NoInvolution.MassageShop.Web/WebModule.cs
@@ -0,0 +1,10 @@
+﻿using MiCake.AspNetCore.Modules;
+using MiCake.Core.Modularity;
+
+namespace NoInvolution.MassageShop.Web
+{
+    [RelyOn(typeof(MiCakeAspNetCoreModule))]
+    public class WebModule : MiCakeModule
+    {
+    }
+}
